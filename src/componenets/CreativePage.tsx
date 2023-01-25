de07638bd346c6a0437085ced4d43d21ec5f99a3
grace k <grace.kuperman5472@gmail.com>
2023-01-25 12:29:12 +0000

    crative coding proj added

--- src/componenets/CreativePage.tsx
+++ src/componenets/CreativePage.tsx
@@ -2,7 +2,20 @@
 function CreativePage(): JSX.Element{
 
     return (
-        <>I am creative page </>
+        <>
+        <h2>Over the last few years Open processing has been a vital tool for applying new cocepts in a fun and imaginatve way.  </h2>
+        <div>
+            <h3> "Iso-Groovy-Lines" </h3>
+            <iframe src="https://openprocessing.org/sketch/1785912/embed/" width="400" height="400"> </iframe>
+            <p> A piece of work adapted from an example of Isolines. Scale of the lines displayed adjusted by user's mouse movements. Knowledge gained includes use of dynamic data.</p>
+        </div>
+        <div>
+            <h3> "Random Mover</h3>
+            <iframe src="https://openprocessing.org/sketch/1785898/embed/" width="400" height="400"></iframe>
+            <p> On eof my first projects back in Open Processing, practicing randomised movement! </p>
+        </div>
+        </>
+        
     )
 }
 
