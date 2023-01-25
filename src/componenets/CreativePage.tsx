3e408b787cc06e7af788cf6c672be7b8e11a01a0
grace k <grace.kuperman5472@gmail.com>
2023-01-25 23:22:58 +0000

    updated crative page css

--- src/componenets/CreativePage.tsx
+++ src/componenets/CreativePage.tsx
@@ -1,18 +1,20 @@
-
+import "./CreativePage.css"
 function CreativePage(): JSX.Element{
 
     return (
         <>
         <h2>Over the last few years Open processing has been a vital tool for applying new cocepts in a fun and imaginatve way.  </h2>
-        <div>
-            <h3> "Iso-Groovy-Lines" </h3>
-            <iframe src="https://openprocessing.org/sketch/1785912/embed/" width="400" height="400"> </iframe>
-            <p> A piece of work adapted from an example of Isolines. Scale of the lines displayed adjusted by user's mouse movements. Knowledge gained includes use of dynamic data.</p>
-        </div>
-        <div>
-            <h3> "Random Mover</h3>
-            <iframe src="https://openprocessing.org/sketch/1785898/embed/" width="400" height="400"></iframe>
-            <p> On eof my first projects back in Open Processing, practicing randomised movement! </p>
+        <div className="ctn-art">
+            <div className="iso-groove">
+                <h3> "Iso-Groovy-Lines" </h3>
+                <iframe src="https://openprocessing.org/sketch/1785912/embed/" width="400" height="400"> </iframe>
+                <p> A piece of work adapted from an example of Isolines. Scale of the lines displayed adjusted by user's mouse movements. Knowledge gained includes use of dynamic data.</p>
+            </div>
+            <div className="random-mover">
+                <h3> "Random Mover</h3>
+                <iframe src="https://openprocessing.org/sketch/1785898/embed/" width="400" height="400"></iframe>
+                <p> One of my first projects back in Open Processing, practicing randomised movement! </p>
+            </div>
         </div>
         </>
         
