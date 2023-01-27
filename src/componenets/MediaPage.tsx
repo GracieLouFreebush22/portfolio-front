6d55280e765d5812bd327f365553b455be4ad317
grace k <grace.kuperman5472@gmail.com>
2023-01-27 13:43:25 +0000

    media data updated

--- src/componenets/MediaPage.tsx
+++ src/componenets/MediaPage.tsx
@@ -1,6 +1,5 @@
-import { mediaDataArr } from "../utils/mediaData"
 import { IMediaData } from "../utils/interfaces"
-import { useState } from "react"
+
 
 interface MediaPageProps {
     picsForMediaPage: IMediaData[];
@@ -9,14 +8,13 @@ function MediaPage({picsForMediaPage}: MediaPageProps): JSX.Element{
     return(
         <>
         <div>
-            <h2> Media </h2>
             { picsForMediaPage.map((pic) => {
                 return (
                     <ul key={pic.id}>
-                        <li>
+                        <ul>
                             <h2> {pic.title}</h2>
-                            <img src={pic.src} alt="" width="400px" height="500px"/>
-                        </li> 
+                            <img src={pic.src} alt="" width="600px" height="500px"/>
+                        </ul> 
                     </ul>
                 )
             })
--- src/utils/mediaData.ts
+++ src/utils/mediaData.ts
@@ -1,9 +1,46 @@
+import geyserIceland from "../images/mediaFldr/geyserIceland.jpg"
+import giantsCauseway from "../images/mediaFldr/giantscauseway.jpg"
+import hugIceland from "../images/mediaFldr/hugIceland.jpg"
+import icelandPony from "..images/mediaFldr/icelandPony.jpg"
+import krakow from "..images/mediaFldr/krakow.jpg"
+import museumIceland from "..images/mediaFldr/museumIceland.jpg"
+
 
 export const mediaDataArr = [
     {
         id: 0,
         title: "Geyser",
         location: "Iceland",
-        src: "/images/mediaFldr/geyserIceland.jpg"
-    }
+        src: geyserIceland
+    },
+    {
+        id: 1,
+        title: "Stepping Stones",
+        location: "Giants Causeway",
+        src: giantsCauseway
+    },
+    {
+        id: 2,
+        title: "Sisters",
+        location: "Iceland",
+        src: hugIceland
+    },
+    {
+        id: 3,
+        title: "Pony",
+        location: "Somewhere in Iceland",
+        src: icelandPony
+    },
+    {
+        id: 4,
+        title: "Horse and Carriage",
+        location: "Krakow, Poland",
+        src: krakow
+    },
+    {
+        id: 5,
+        title: "Art Museum",
+        location: "Rejkavik, Iceland",
+        src: museumIceland
+    },
 ]
