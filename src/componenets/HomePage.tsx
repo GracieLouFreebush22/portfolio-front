96fb3006f2bc065424551502c5796cb5e1b9e98f
grace k <grace.kuperman5472@gmail.com>
2023-02-02 16:08:43 +0000

    home page started

--- src/componenets/HomePage.tsx
+++ src/componenets/HomePage.tsx
@@ -1,32 +1,46 @@
+import { useState } from "react";
 import { languagesArr } from "../utils/languages";
+import { fullStackDataArr } from "../utils/fullStackData";
+import { mediaDataArr } from "../utils/mediaData";
+interface IFullStackPrevProps {
+  id: number;
+  title: string;
+  img: string;
+}
 
-function HomePage(): JSX.Element {
-  // const handleHome = () => {
-  //   console.log("i am home button");
-  // };
-  // const handleFullStack = () => {
-  //   console.log("I am full stack button");
-  // };
-  // const handleCreative = () => {
-  //   console.log("I am creative button");
-  // };
-  // const handleTeaching = () => {
-  //   console.log("I am teaching button");
-  // };
+interface ICreativePreviewProps {
+  id: number;
+  title: string;
+  img: string;
+}
+function HomePage(props: IFullStackPrevProps): JSX.Element {
+const [fullStackPreview, setFullStackPreview]= useState<IFullStackPrevProps[]>([])
+const [mediaPreview, setMediaPreview] = useState<ICreativePreviewProps[]>([])
 
+const getFullStackPreview = async () => {
+  setFullStackPreview(fullStackDataArr)
+}
+
+const getCreativePagePreview = async () => {
+  setMediaPreview(mediaDataArr)
+}
   return (
     <>
       <div>
         <h2> Explore Content... </h2>
       </div>
-      <>
+      <div>
        {languagesArr.map((el,i) => {
         return (
         <span key={i}> 🌟 {el} 🌟</span> 
         )
       })}
        
-      </>
+      </div>
+      {/* --------------------------------projects sneak peek */}
+      < FullStackPreview fullStackPreview ={fullStackPreview}/>
+      {/* -------------------------------- creative sneak peek */}
+      < MediaPreview mediaPreview = {mediaPreview}/>
     </>
   );
 }
