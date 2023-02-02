50611eecd35c135e4fea56cad7274cafc695dcc8
grace k <grace.kuperman5472@gmail.com>
2023-02-02 13:37:12 +0000

    images still not working

--- src/componenets/MediaPage.tsx
+++ src/componenets/MediaPage.tsx
@@ -1,22 +1,28 @@
-import { mediaDataArr } from "../utils/mediaData";
+import { mediaDataArr } from "./mediaData";
+import ponySmall from "../images/ponySmall.jpg"
+import "./MediaPage.css";
+
 
 function MediaPage(): JSX.Element{
     return(
         <>
         <div>
+            <img src="PonySmall" alt=""/>
+        </div>
+        {/* <div>
             { mediaDataArr.map((pic) => {
                 return (
-                    <div key={pic.id}>
-                     <h2> {pic.title}</h2>
+                    <div className="media-ctn" key={props.id}>
+                     <h2> {props.title}</h2>
                        <div>
-                        <img src={pic.img} alt="" width="800px" height="500px"/>
+                        <img src={props.img} alt="" className="media-img"/>
                       </div>
                     </div>
                 )
             })
 
             }
-        </div> 
+        </div>  */}
       
         </>
     )
