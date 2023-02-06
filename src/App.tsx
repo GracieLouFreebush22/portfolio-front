0d24b2828be975a4ba228675ba1d062da1e3cca4
grace k <grace.kuperman5472@gmail.com>
2023-02-06 13:06:52 +0000

    app prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
 import HomePage from "./componenets/HomePage";
 import FullStackPage from "./componenets/FullStackPage";
 import CreativePage from "./componenets/CreativePage";
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 import TeachingPage from "./componenets/TeachingPage";
 import MediaPage from "./componenets/MediaPage";
-import AboutPage from "./componenets/AboutPage"
-import "./App.css"
-
- 
-
+import AboutPage from "./componenets/AboutPage";
+import "./App.css";
 
 function App(): JSX.Element {
-
-
   return (
-    <div className= "ctn-app">
+    <div className="ctn-app">
       <Routes>
-        <Route path="/" element={<HomePage  />} />
-        <Route path="/Projects" element={<FullStackPage/>}/>
-        <Route path ="/Creative" element={<CreativePage/>}/>
-        <Route path="/Teaching" element={<TeachingPage/>}/>
-        <Route path="/Media" element={<MediaPage />}/>
-        <Route path="/About" element={<AboutPage />}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Projects" element={<FullStackPage />} />
+        <Route path="/Creative" element={<CreativePage />} />
+        <Route path="/Teaching" element={<TeachingPage />} />
+        <Route path="/Media" element={<MediaPage />} />
+        <Route path="/About" element={<AboutPage />} />
       </Routes>
-      
     </div>
-    
-  )
-  
- 
+  );
 }
 
 export default App;
