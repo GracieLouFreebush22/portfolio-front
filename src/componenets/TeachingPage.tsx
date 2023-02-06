828962a669ae514ac43ca061b3e3d3c60a39c7ac
grace k <grace.kuperman5472@gmail.com>
2023-02-06 15:09:24 +0000

    teaching page about included

--- src/componenets/TeachingPage.tsx
+++ src/componenets/TeachingPage.tsx
@@ -1,12 +1,21 @@
+import "../css/TeachingPage.css"
+
 function TeachingPage(): JSX.Element {
   return (
     <>
+    <div className="teaching-abt">
       <h2 className="ctn-teach-header">
-        {" "}
-        Welcome! I have spent the last three years working part-time as a coding
-        teacher both in person and online in London,UK and in Houston, Tx. Here
-        are some of the projects I have made for my students!
-      </h2>
+          {" "}
+          Welcome!
+        </h2>
+        <p>
+          I have spent the last three years working part-time as a coding
+          teacher both in person and online in London,UK and in Houston, Tx. During this time, I noticed that teaching staff 
+          find it challenging to integrate computer programming into their curriuculum because it requires longer-term training for teaching staff. 
+          Here are some of the projects I have made for my students!
+        </p>
+    </div>
+     
       <div>
         <iframe
           src="https://scratch.mit.edu/projects/757610373/embed"
