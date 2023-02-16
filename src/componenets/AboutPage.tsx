8390730fc50e6fd4c4d8d92abaa40c4c34d7036f
grace k <grace.kuperman5472@gmail.com>
2023-02-16 16:43:42 +0000

    media page filled out

--- src/componenets/AboutPage.tsx
+++ src/componenets/AboutPage.tsx
@@ -1,7 +1,16 @@
+import "../css/AboutMePage.css"
 function AboutPage(): JSX.Element {
   return (
     <>
       <h1> Welcome to my Online Portfolio! </h1>
+      <div className= "about-me-bio-ctn">
+      <p> Moved to London from Houston, Texas in 2016 to attend University </p>
+      <p>I am passionate about Education, Women in Tech, and International Politics.
+        I have spent the last two years building my software engineering skills
+        as I am working to combine my background in the social sciences with my knowledge and experience in the Technology Industry.
+        I am fascinated by the stories driven by innovation, and hope to work in an environment that welcomes change, 
+        and is dedicated to growing its social impact  </p>
+      </div>
     </>
   );
 }
--- src/utils/mediaData.ts
+++ src/utils/mediaData.ts
@@ -5,7 +5,17 @@ import ponySmall from "../images/ponySmall.png";
 import krakowSmall from "../images/krakowSmall.jpg";
 import museumSmall from "../images/museumSmall.jpg";
 import parisSmall from "../images/parisSmall.jpg";
-
+import lucy from "../images/lucy.jpg";
+import empireStateBuilding from "../images/empireStateBuilding.jpg"
+import niceUmbrellas from "../images/niceUmbrellas.jpg"
+import disneyWorld from "../images/disneyWorld.jpg"
+import jazzBand from "../images/jazzBand.jpg"
+import spainStreet from "../images/spainStreet.jpg"
+import berlinContrast from "../images/berlinContrast.jpg"
+import mummumsBath from "../images/mummumsBath.jpg"
+import amsterdam from "../images/amsterdam.jpg"
+import saturated from "../images/saturated.jpg"
+import windyJump from "../images/windyJump.jpg"
 export const mediaDataArr = [
   {
     id: 0,
@@ -48,5 +58,66 @@ export const mediaDataArr = [
     title: "Eiffel Tower",
     location: "Paris, France",
     img: parisSmall,
+  },
+  {
+    id: 7,
+    title: "Lucy",
+    location: "Texas",
+    img: lucy,
+  },
+  {
+    id: 8,
+    title: "Amsterdam",
+    location: "The Hague",
+    img: amsterdam,
+  },
+  {
+    id: 9,
+    title: "Umbrellas",
+    location: "Nice, France",
+    img: niceUmbrellas,
+  },
+  {
+    id: 10,
+    title: "Disney World",
+    location: "Orlando, Florida",
+    img: disneyWorld,
+  },
+  {
+    id: 11,
+    title: "Empire State Building",
+    location: "New York City",
+    img: empireStateBuilding,
+  },
+  {
+    id: 12,
+    title: "Seville",
+    location: "Spain",
+    img: saturated,
+  },
+  {
+    id: 13,
+    title: "Bath in Time",
+    location: "Bath England",
+    img: mummumsBath,
+  },
+  {
+    id: 14,
+    title: "Berlin",
+    location: "Germany",
+    img: berlinContrast,
+   
+  },
+  {
+    id: 15,
+    title: "Streets of Seville",
+    location: "Spain",
+    img: spainStreet,
+  },
+  {
+    id: 16,
+    title: "A WIndy Jump",
+    location: "Brancaster England",
+    img: windyJump,
   },
 ];
